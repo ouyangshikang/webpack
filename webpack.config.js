206c06ce35542fa983fc31de79470361a8ba49d6
oysk <1062628791@qq.com>
2017-12-11 00:02:05 +0800

    webpack-dev-server  &  loader

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const uglify = require('uglifyjs-webpack-plugin');
+module.exports = {
+    entry: {
+        entry: './src/entry.js',
+        entry1: './src/entry1.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }],
+            }
+        ]
+    },
+    plugins: [
+        new uglify()
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        host: '127.0.0.1',
+        compress: true,
+        port: 9090
+    }
+}
