784e44f443b290bd77110a073d66cc11a4e8c2dc
oysk <1062628791@qq.com>
2017-12-12 00:54:27 +0800

    处理css图片及路径问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const uglify = require('uglifyjs-webpack-plugin');
 const htmlPlugin = require('html-webpack-plugin');
+const extractTextPlugin  = require('extract-text-webpack-plugin');
+const website = {
+    publicPath: "http://localhost:9090/"
+}
 
 module.exports = {
     entry: {
@@ -9,17 +13,22 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js'
+        filename: '[name].js',
+        publicPath: website.publicPath
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: [{
-                    loader: 'style-loader'
-                }, {
-                    loader: 'css-loader'
-                }],
+                // use: [{
+                //     loader: 'style-loader'
+                // }, {
+                //     loader: 'css-loader'
+                // }],
+                use: extractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
             },
             {
                 test: /\.js$/,
@@ -29,7 +38,16 @@ module.exports = {
                      presets: ['es2015']
                   }
                 }],
-                exclude: /node_modules/
+                // exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpg|gif)/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 5000
+                    }
+                }]
             }
         ]
     },
@@ -41,7 +59,8 @@ module.exports = {
             },
             hash: true, //生成hash值
             template: './src/index.html'   // 以这个html为模板
-        })
+        }),
+        new extractTextPlugin("/css/index.css")
     ],
     devServer: {
         contentBase: path.resolve(__dirname, 'dist'),
